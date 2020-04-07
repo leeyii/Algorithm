4eca199ff7133966ce7716cffacf8fcdb94a4865
leeyii <>
2020-04-07 22:47:30 +0800

    双向循环链表

--- 数据结构与算法/005-双向循环链表/005-双向循环链表/main.c
+++ 数据结构与算法/005-双向循环链表/005-双向循环链表/main.c
@@ -56,11 +56,53 @@ Status ListInsert(LinkList *l, int index, Element e) {
     
     if (*l == NULL || index < 0) return ERROR;
     
-    Node *p = (*l)->next;
+    Node *p = (*l);
     
     for (int i = 0; i < index; i++) {
-        
+        p = p->next;
+        if (p == *l) {
+            printf("插入位置超过链表的长度\n");
+            return ERROR;
+        };
     }
+    
+    Node *n = malloc(sizeof(Node));
+    if (!n) return ERROR;
+    n->data = e;
+    
+    n->next = p->next;
+    p->next->prior = n;
+    
+    p->next = n;
+    n->prior = p;
+    
+    return OK;
+}
+
+
+Status ListDelete(LinkList *l, int index, Element *e) {
+    
+    if (*l == NULL || index < 0) return ERROR;
+    
+    Node *p = *l;
+    
+    if (p->next == p) {
+        printf("链表为空\n");
+        return ERROR;
+    }
+    
+    for (int i = 0; i <= index; i++) {
+        p = p->next;
+        if (p == *l) {
+            printf("删除位置超过链表长度\n");
+            return ERROR;
+        }
+    }
+    
+    p->prior->next = p->next;
+    p->next->prior = p->prior;
+    *e = p->data;
+    free(p);
     
     return OK;
 }
@@ -86,7 +128,6 @@ void ShowList(LinkList list) {
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
-<<<<<<< HEAD
     
     Status iStatus;
     LinkList list;
@@ -96,8 +137,33 @@ int main(int argc, const char * argv[]) {
     
     ShowList(list);
     
+    // 插入链表
+//    ListInsert(&list, 11, 99);
+//    ShowList(list);
+//
+//    ListInsert(&list, 9, 99);
+//    ShowList(list);
+//
+//    ListInsert(&list, 11, 99);
+//    ShowList(list);
+//
+//    ListInsert(&list, 13, 99);
+//    ShowList(list);
+    
+    int a;
+    // 删除
+    ListDelete(&list, 9, &a);
+    printf("%d\n", a);
+    ShowList(list);
+    
+//    ListDelete(&list, 0, &a);
+//    printf("%d\n", a);
+//    ShowList(list);
+//
+//    ListDelete(&list, 0, &a);
+//    printf("%d\n", a);
+//    ShowList(list);
+    
+    
     return 0;
-=======
-    return 0;//
->>>>>>> 920f213147ebc4706dae697995b8d16d34d24ae6
 }
