477f8280ae02fdfff230b1613ad1a1231882a059
leeyii <>
2020-04-07 21:58:05 +0800

    双向循环链表

--- 数据结构与算法/005-双向循环链表/005-双向循环链表/main.c
+++ 数据结构与算法/005-双向循环链表/005-双向循环链表/main.c
@@ -7,9 +7,93 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+#define OK 1
+#define ERROR 0
+
+typedef int Element;
+typedef int Status;
+
+typedef struct Node {
+    Element data;
+    struct Node *next;
+    struct Node *prior;
+}Node, *LinkList;
+
+
+// 创建双向链表
+Status CreateList(LinkList *l) {
+    
+    // 创建头结点
+    *l = (LinkList)malloc(sizeof(Node));
+    if (*l == NULL) return ERROR;
+    (*l)->next = *l;
+    (*l)->prior = *l;
+    
+    // 尾结点指针
+    Node *tail = *l;
+    
+    for (int i = 0; i < 10; i++) {
+        Node *n = malloc(sizeof(Node));
+        n->data = i;
+        
+        
+        tail->next->prior = n;
+        n->next = tail->next;
+        
+        n->prior = tail;
+        tail->next = n;
+        
+        tail = n;
+    }
+    
+    return OK;
+}
+
+//插入元素
+Status ListInsert(LinkList *l, int index, Element e) {
+    
+    if (*l == NULL || index < 0) return ERROR;
+    
+    Node *p = (*l)->next;
+    
+    for (int i = 0; i < index; i++) {
+        
+    }
+    
+    return OK;
+}
+
+
+void ShowList(LinkList list) {
+    
+    if (!list || list->next == list) return;
+    
+    printf("链表的内容为:");
+    
+    Node *p = list->next;
+    
+    while (p != list) {
+        printf(" %d", p->data);
+        p = p->next;
+    }
+    
+    printf("\n");
+}
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
+    
+    Status iStatus;
+    LinkList list;
+    
+    // 创建
+    iStatus = CreateList(&list);
+    
+    ShowList(list);
+    
     return 0;
 }
