698a9555dac827889bb9a413bfb0346f2d9b8369
leeyii <lijie_666666@126.com>
2020-04-08 11:07:13 +0800

    双向循环链表‘

--- 数据结构与算法/005-双向循环链表/005-双向循环链表/main.c
+++ 数据结构与算法/005-双向循环链表/005-双向循环链表/main.c
@@ -35,16 +35,18 @@ Status CreateList(LinkList *l) {
     Node *tail = *l;
     
     for (int i = 0; i < 10; i++) {
+        // 创建新的结点
         Node *n = malloc(sizeof(Node));
         n->data = i;
-        
-        
-        tail->next->prior = n;
-        n->next = tail->next;
-        
+        // 1. 尾结点的后继的前驱指向新建结点, 相当于头结点的前驱指向 n
+        tail->next->prior = n; // (*l)->prior = n;
+        // 2. 新建结点的后继指向尾结点的后继, 相当于n的后继指向 头结点
+        n->next = tail->next; // n->next = (*l);
+        // 3. 新建结点的前驱指向尾结点
         n->prior = tail;
+        // 4. 尾结点的后置指向新建结点
         tail->next = n;
-        
+        // 5. 尾指针指向新建结点
         tail = n;
     }
     
@@ -57,9 +59,10 @@ Status ListInsert(LinkList *l, int index, Element e) {
     if (*l == NULL || index < 0) return ERROR;
     
     Node *p = (*l);
-    
+    // 寻找插入位置的前一个结点
     for (int i = 0; i < index; i++) {
         p = p->next;
+        // 遍历一遍, 还没有到插入位置, 直接return
         if (p == *l) {
             printf("插入位置超过链表的长度\n");
             return ERROR;
@@ -69,11 +72,13 @@ Status ListInsert(LinkList *l, int index, Element e) {
     Node *n = malloc(sizeof(Node));
     if (!n) return ERROR;
     n->data = e;
-    
+    // 插入结点的后继指向插入结点前驱的后继
     n->next = p->next;
+    // 插入结点后继的前驱指向插入结点
     p->next->prior = n;
-    
+    // 插入结点前驱的后继指向插入结点
     p->next = n;
+    // 插入结点前驱指向p
     n->prior = p;
     
     return OK;
@@ -90,7 +95,7 @@ Status ListDelete(LinkList *l, int index, Element *e) {
         printf("链表为空\n");
         return ERROR;
     }
-    
+    // 查找删除的元素
     for (int i = 0; i <= index; i++) {
         p = p->next;
         if (p == *l) {
@@ -98,12 +103,13 @@ Status ListDelete(LinkList *l, int index, Element *e) {
             return ERROR;
         }
     }
-    
+    // 删除结点的前驱的后继指向删除结点的后继
     p->prior->next = p->next;
+    // 删除结点的后继的前驱指向删除结点的前驱
     p->next->prior = p->prior;
     *e = p->data;
+    // 释放删除结点内存
     free(p);
-    
     return OK;
 }
 
