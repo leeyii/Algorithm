a572c3e6e01fc6651df8707c83b6029186f7eb19
leeyii <lijie_666666@126.com>
2020-04-14 17:02:56 +0800

    队列的实现

--- 数据结构与算法/007-栈/007-栈/main.c
+++ 数据结构与算法/007-栈/007-栈/main.c
@@ -21,7 +21,7 @@ typedef int Status;
 typedef int Element;
 
 typedef struct {
-    int* data;
+    Element* data;
     int top;
     int maxSize;
 }SqStack, *SqStackP;
@@ -31,13 +31,16 @@ Status initStack(SqStackP s) {
     
     s->data = (Element *)malloc(sizeof(Element)*SIZE);
     if (s->data == NULL) return ERROR;
+    // 初始栈顶索引
     s->top = -1;
+    // 栈的最大容量, 用于扩容.
     s->maxSize = SIZE;
     return OK;
 }
 
 /// 清空栈
 Status clearStack(SqStackP s) {
+    // 将栈顶索引置为-1
     s->top = -1;
     return OK;
 }
@@ -51,15 +54,14 @@ Status stackIsEmpty(SqStack s) {
     }
 }
 
-/// 判断栈的长度
-
-Status stackLength(SqStack s) {
+/// 获取栈的长度
+int stackLength(SqStack s) {
     return s.top + 1;
 }
 
 /// 获取栈顶元素
 Status getStackTop(SqStack s, Element *e) {
-    
+    // 如果是空栈 return
     if (s.top == -1) return ERROR;
     *e = s.data[s.top];
     return OK;
@@ -68,8 +70,9 @@ Status getStackTop(SqStack s, Element *e) {
 /// 入栈
 Status push(SqStackP s, Element e) {
     
+    // 如果顺序栈容量达到最大值
     if (s->top == (s->maxSize - 1)) {
-        // 栈满了
+        // 扩容
         s->data = realloc(s->data, (s->maxSize + SIZE));
         if (s->data == NULL) return ERROR;
         s->maxSize += SIZE;
@@ -88,6 +91,17 @@ Status pop(SqStackP s, Element *e) {
     return OK;
 }
 
+/// 销毁栈
+Status destoryStack(SqStackP s) {
+    // 释放数组的空间
+    free(s->data);
+    
+    s->top = -1;
+    s->maxSize = 0;
+    return OK;
+}
+
+
 /// 从栈低到栈顶遍历
 void stackTraverse(SqStack s) {
     if (stackIsEmpty(s)) return;
@@ -105,9 +119,13 @@ int main(int argc, const char * argv[]) {
     SqStack stack;
     int e;
     
-    initStack(&stack);
+    if (initStack(&stack)) {
+        printf("顺序栈初始化成功\n");
+    } else {
+        printf("顺序栈初始化失败\n");
+    }
     
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 20; i++) {
         push(&stack, i);
     }
     printf("顺序栈中元素为:\n");
@@ -122,9 +140,13 @@ int main(int argc, const char * argv[]) {
     stackTraverse(stack);
     printf("是否为空栈:%d\n",stackIsEmpty(stack));
     printf("栈的长度为:%d\n", stackLength(stack));
+    printf("清空栈\n");
     clearStack(&stack);
     printf("是否已经清空栈 %d, 栈长度为:%d\n",stackIsEmpty(stack), stackLength(stack));
     
+    if (destoryStack(&stack)) {
+        printf("栈销毁成功\n");
+    }
     
     return 0;
 }
--- 数据结构与算法/008-队列/008-队列(链表实现)/main.c
+++ 数据结构与算法/008-队列/008-队列(链表实现)/main.c
@@ -21,36 +21,144 @@ typedef int Status;
 typedef struct QNode {
     QElemType data;
     struct QNode *next;
-}QNode, QNodePtr;
+}QNode, *QNodePtr;
 
 typedef struct {
     QNodePtr front;
     QNodePtr rear;
+    int count;
 } LinkQueue;
 
 // 1.初始化
 Status initQueue(LinkQueue *Q) {
+    if (Q == NULL) return ERROR;
+    // 初始化头结点
     Q->rear = Q->front = (QNodePtr)malloc(sizeof(QNode));
     if (Q->front == NULL) return ERROR;
-    Q->front = NULL;
+    Q->front->next = NULL;
+    Q->count = 0;
     return OK;
 }
 
 // 2.销毁队列
 Status destoryQueue(LinkQueue *Q) {
-    
+    if (!Q) return ERROR;
+    // 从头结点开始依次释放结点
+    while (Q->front) {
+        Q->rear = Q->front->next;
+        Q->front = Q->front->next;
+        free(Q->rear);
+    }
+    Q->count = 0;
     return OK;
 }
 
 // 3.清空队列
 Status clearQueue(LinkQueue *Q) {
-    
-    
+    if (!Q) return ERROR;
+    // 保留头结点,释放其余结点
+    QNodePtr p, q;
+    // 指向首元结点
+    p = Q->front->next;
+    while (p) {
+        q = p;
+        p = p->next;
+        free(q);
+    }
+    Q->front->next = NULL;
+    Q->rear = Q->front;
+    Q->count = 0;
     return OK;
+}
+
+// 4.队列是否为空
+Status queueEmpty(LinkQueue Q) {
+    return Q.front == Q.rear;
+}
+
+// 5.获取队列的长度
+int queueLength(LinkQueue Q) {
+    return Q.count;
+}
+
+// 6.获取队头
+Status getQueueHead(LinkQueue Q, QElemType *e) {
+    *e = Q.front->next->data;
+    return OK;
+}
+
+// 7.入队
+Status enqueue(LinkQueue *Q, QElemType e) {
+    if (!Q) return ERROR;
+    QNodePtr p = (QNodePtr)malloc(sizeof(QNode));
+    if (!p) return ERROR;
+    p->next = NULL;
+    p->data = e;
+    Q->rear->next = p;
+    Q->rear = p;
+    Q->count++;
+    return OK;
+}
+
+// 8.出队
+Status dequeue(LinkQueue *Q, QElemType *e) {
+    if (!Q) return ERROR;
+    if (Q->count == 0) return ERROR;
+    // 获取队头结点的值
+    *e = Q->front->next->data;
+    // 获取队头结点
+    QNodePtr p = Q->front->next;
+    Q->front->next = p->next;
+    free(p);
+    Q->count--;
+    // 删空后,将队尾指向头结点
+    if (Q->count == 0) Q->rear = Q->front;
+    return OK;
+}
+
+// 9.遍历
+void queueTraverse(LinkQueue Q) {
+    
+    QNodePtr p = Q.front->next;
+    
+    while (p) {
+        printf("%d  ", p->data);
+        p = p->next;
+    }
+    printf("\n");
 }
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
+    
+    LinkQueue q;
+    int e;
+    
+    if (initQueue(&q)) {
+        printf("队列初始化成功\n");
+    }
+    for (int i = 0; i < 10; i++) {
+        enqueue(&q, i);
+    }
+    printf("队列中的元素为:");
+    queueTraverse(q);
+    printf("队列的长度为: %d\n", queueLength(q));
+    printf("队列是否为空: %d\n", queueEmpty(q));
+    dequeue(&q, &e);
+    printf("出队元素为: %d\n", e);
+    getQueueHead(q, &e);
+    printf("获取队头: %d\n", e);
+    printf("队列中的元素为:");
+    queueTraverse(q);
+    printf("队列的长度为: %d\n", queueLength(q));
+    printf("队列是否为空: %d\n", queueEmpty(q));
+    clearQueue(&q);
+    printf("队列中的元素为:");
+    queueTraverse(q);
+    printf("队列的长度为: %d\n", queueLength(q));
+    printf("队列是否为空: %d\n", queueEmpty(q));
+    destoryQueue(&q);
+    
     return 0;
 }
