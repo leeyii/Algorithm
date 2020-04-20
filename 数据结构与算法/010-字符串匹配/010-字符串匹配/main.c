a70748b01f437ca6febcf0a7a49f5a087db44c71
leeyii <leeyii@leeyiideMacBook-Pro-2.local>
2020-04-20 20:47:43 +0800

    字符串匹配

---
+++ 数据结构与算法/010-字符串匹配/010-字符串匹配/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  010-字符串匹配
+//
+//  Created by leeyii on 2020/4/20.
+//  Copyright © 2020 leeyii. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
